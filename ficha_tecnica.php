275df0f5f2af16b77ab9149f44a44b8e3477c261
Thiago Silva <37704790+thiago-z@users.noreply.github.com>
2018-06-25 12:24:59 -0300

    Update ficha_tecnica.php

--- ficha_tecnica.php
+++ ficha_tecnica.php
@@ -123,77 +123,9 @@ break;
 </footer>	
 
 </main>
-<script language="Javascript">
-
-	function myFunction() {	
-		
-	var element = document.getElementById("lista");	
-	element.classList.toggle("info_ativa");	
 	
-	var element = document.getElementById("lista2");	
-	element.classList.remove("info_ativa");		
-		
-	var element = document.getElementById("lista3");	
-	element.classList.remove("info_ativa");		
-
-	var element = document.getElementById("info");
-	element.classList.toggle("invisivel");
-		
-	var element = document.getElementById("info2");
-	element.classList.add("invisivel");		
-			
-	var element = document.getElementById("info3");
-	element.classList.add("invisivel");
-		
-}
+<script src="js/filtro_titulos.jf"></script>
 	
-	
-	
-	function myFunction2() {
-		
-	var element = document.getElementById("lista");	
-	element.classList.remove("info_ativa");	
-	
-	var element = document.getElementById("lista2");	
-	element.classList.toggle("info_ativa");		
-		
-	var element = document.getElementById("lista3");	
-	element.classList.remove("info_ativa");	
-			
-	 var element = document.getElementById("info");
-	element.classList.add("invisivel");	
-		
-    var element = document.getElementById("info2");
-	element.classList.toggle("invisivel");
-		
-		
-		
-	 var element = document.getElementById("info3");
-	element.classList.add("invisivel");	
-}
-	
-	function myFunction3() {
-		
-	var element = document.getElementById("lista");	
-	element.classList.remove("info_ativa");	
-	
-	var element = document.getElementById("lista2");	
-	element.classList.remove("info_ativa");		
-		
-	var element = document.getElementById("lista3");	
-	element.classList.toggle("info_ativa");		
-				
-	 var element = document.getElementById("info");
-	element.classList.add("invisivel");		
-					
-    var element = document.getElementById("info2");
-	element.classList.add("invisivel");	
-		
-		
-	 var element = document.getElementById("info3");
-	element.classList.toggle("invisivel");	
-}
-	
-</script>
 </body>
+	
 </html>
